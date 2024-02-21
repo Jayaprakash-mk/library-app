a656b3c33188628e03cb186223d5e8880eedeba8
Jayaprakash MK <mk.jayaprakash.maniraj@gmail.com>
2024-02-21 08:14:22 +0530

    app pushed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
+
 import './App.css';
+import React, {useState}from "react";
+import AddBookForm from './frontend/addBookForm.js';
+import HomePage from './frontend/HomePage.js';
+import BookList from './frontend/BookList';
+
 
 function App() {
+  const [Add, AddNewData] = useState(false);
+  const [View, ViewNewData] = useState(false);
+
+  const AddData = (okay) => {
+    AddNewData(okay);
+  }
+  const ViewData = (okay) => {
+    ViewNewData(okay);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {!Add && !View && <HomePage choice1={AddData} choice2={ViewData}/>}
+      {Add && !View && <AddBookForm/>}
+      {View && !Add && <BookList/>}
+    </>
   );
 }
 
