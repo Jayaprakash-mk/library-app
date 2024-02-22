a3236950db0e5df5148110dad84e6268b8c3413f
Jayaprakash MK <mk.jayaprakash.maniraj@gmail.com>
2024-02-23 00:46:41 +0530

    Modified code

--- backend/app.js
+++ backend/app.js
@@ -4,7 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const cors  = require('cors');
-var indexRouter = require('./routes/index');
+var userRouter = require('./routes/users');
 //var usersRouter = require('./routes/users');
 
 var app = express();
@@ -19,7 +19,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+app.use('/books', userRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,12 +1,12 @@
 var express = require('express');
 var router = express.Router();
 var usersRouter = require('./users.js');
-const cors  = require('cors');
-const app = express();
-require('dotenv').config();
-app.use(express.json());
-app.use(cors());
-app.use('/books', usersRouter);
+// const cors  = require('cors');
+// const app = express();
+// require('dotenv').config();
+// app.use(express.json());
+// app.use(cors());
+router.use('/books', usersRouter);
 
-app.listen(8000, () => console.log("sever running"));
+// app.listen(8000, () => console.log("sever running"));
 module.exports = router;
